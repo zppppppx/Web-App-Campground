bfa0c7c649c41e5f0ddc71cb82b7c2aab8612783
zppppppx <zppppppx@gmail.com>
2022-12-30 13:12:45 -0800

    Set up routes, static methods for validating forms, session for cookies

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // Basic Framework
 const express = require('express');
-const path = require('path')
+const path = require('path');
 const mongoose = require('mongoose');
-const methodOverride = require('method-override')
-const ejsMate = require('ejs-mate')
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
 
 // Validation and Error
 const ExpressError = require('./utils/ExpressError');
@@ -15,6 +16,7 @@ const reviews = require('./routes/reviews');
 
 // Connecting with the database
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
+mongoose.set('strictQuery', false); // prepare for mongoose 7
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection Error:"));
@@ -28,13 +30,26 @@ const app = express()
 app.engine('ejs', ejsMate)
 app.use(express.urlencoded({ extended: true })) // for parsing request object
 app.use(methodOverride('_method'));
-app.use(express.static('public')) // Setting the static response directory.
+app.use(express.static(path.join(__dirname, 'public'))); // Setting the static response directory.
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+// Setting express session
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true,
+    }
+}
+app.use(session(sessionConfig));
+
 
 app.use('/campgrounds', campgrounds); // campground site
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/campgrounds/:id/reviews', reviews); // review site
 
 app.get('/', (req, res) => {
     res.render('home')
