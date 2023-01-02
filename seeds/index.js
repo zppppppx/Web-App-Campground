6f99ad5da116acbe5f09487588c7a7a7f02342fb
zppppppx <zppppppx@gmail.com>
2023-01-01 20:38:20 -0800

    1. Modified the seeds to randomly generate the campgrounds.

--- seeds/index.js
+++ seeds/index.js
@@ -26,9 +26,22 @@ const seedDB = async () => {
             author: '63b087f02a274998d11f5caa',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
             price: price,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi adipisci minima quasi est perferendis eos alias aperiam, amet sint nemo enim quibusdam necessitatibus culpa, labore suscipit. Ipsum consectetur at dicta.'
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi adipisci minima quasi est perferendis eos alias aperiam, amet sint nemo enim quibusdam necessitatibus culpa, labore suscipit. Ipsum consectetur at dicta.',
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dhwbsgmjw/image/upload/v1672631516/YelpCamp/iciq0arvtdnvkqi4q6sp.png',
+                    filename: 'YelpCamp/iciq0arvtdnvkqi4q6sp',
+                },
+                {
+                    url: 'https://res.cloudinary.com/dhwbsgmjw/image/upload/v1672631516/YelpCamp/vrdunxjmkybbqgx0bmic.png',
+                    filename: 'YelpCamp/vrdunxjmkybbqgx0bmic',
+                },
+                {
+                    url: 'https://res.cloudinary.com/dhwbsgmjw/image/upload/v1672631517/YelpCamp/tb7mywgd3fnjayy2frap.png',
+                    filename: 'YelpCamp/tb7mywgd3fnjayy2frap',
+                }
+            ]
         })
         await camp.save()
     }
