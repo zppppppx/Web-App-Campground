3e986577d4e1882e52aa07cd40f212d219861695
zppppppx <zppppppx@gmail.com>
2023-01-02 10:00:38 -0800

    1. Modified the campgroud model and added a virtual to its image schema to support image transformation from cloudinary. This is to shrink down the image size in edit form to let them look nicer.

--- models/campground.js
+++ models/campground.js
@@ -3,15 +3,19 @@ const Review = require('./review');
 
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String,
+})
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200,h_150,c_fill'); //using virtual can ease the storage.
+})
+
 const CampgroundSchema = new Schema({
     title: String,
     price: Number,
-    images: [
-        {
-            url: String,
-            filename: String,
-        }
-    ],
+    images: [ImageSchema],
     description: String,
     location: String,
     author: {
@@ -26,11 +30,13 @@ const CampgroundSchema = new Schema({
     ]
 })
 
+https://res.cloudinary.com/dhwbsgmjw/image/upload/w_300/v1672680691/YelpCamp/fhnze88gcpuftwb9om9r.png
+
 
 // This post middleware is binded to what we have used for deleting the campground `findByIdAndDelete`
-CampgroundSchema.post('findOneAndDelete', async function(doc) {
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
     // here doc is what we deleted.
-    if(doc) {
+    if (doc) {
         await Review.deleteMany({
             _id: {
                 $in: doc.reviews,
