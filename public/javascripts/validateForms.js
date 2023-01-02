d74a3e6968981cc3d40322ae0e0e251adfcae5d1
zppppppx <zppppppx@gmail.com>
2023-01-01 22:50:10 -0800

    1. Added the selected images' path-show feature initialization.

--- public/javascripts/validateForms.js
+++ public/javascripts/validateForms.js
@@ -1,6 +1,8 @@
 (function () {
     'use strict'
 
+    bsCustomFileInput.init()
+
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.validated-form') // validated-form is the class of what form we need to validate
 
