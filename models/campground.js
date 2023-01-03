200b11a0ec73118107a92855429747400ea85edb
zppppppx <zppppppx@gmail.com>
2023-01-02 19:31:44 -0800

    1. Added geometry field.

--- models/campground.js
+++ models/campground.js
@@ -22,6 +22,17 @@ const CampgroundSchema = new Schema({
     images: [ImageSchema],
     description: String,
     location: String,
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true,
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+        }
+    },
     author: {
         type: Schema.Types.ObjectId,
         ref: 'User',
