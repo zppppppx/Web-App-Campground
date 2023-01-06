70d1a93699cd1b4637d35b831134385a3b90c835
zppppppx <zppppppx@gmail.com>
2023-01-05 22:25:19 -0800

    1. Added a new feature to reset the password when users forget, including send a reset link (may be updated to email-based if have time);

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,8 @@
 const User = require('../models/user');
+const crypto = require('crypto');
+const UserToken = require('../models/userToken');
+const bcrypt = require('bcrypt');
+const ExpressError = require('../utils/ExpressError');
 
 module.exports.renderRegisterForm = (req, res) => {
     res.locals.username = req.session.username || '';
@@ -15,13 +19,13 @@ module.exports.register = async (req, res, next) => {
         const user = new User({ username, email });
         const registerUser = await User.register(user, password);
         req.login(registerUser, err => {
-            if(err) return next(err);
+            if (err) return next(err);
             delete req.session.username;
             delete req.session.email;
             req.flash('success', 'Welcome to YelpCamp!');
             res.redirect('/campgrounds');
         })
-        
+
     }
     catch (e) {
         req.flash('error', e.message);
@@ -34,11 +38,11 @@ module.exports.renderLoginForm = (req, res) => {
 };
 
 module.exports.login = (req, res) => {
-        req.flash('success', 'Welcome back!');
+    req.flash('success', 'Welcome back!');
 
-        const redirectUrl = req.session.returnTo || '/campgrounds';
-        delete req.session.returnTo;
-        res.redirect(redirectUrl);
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
 };
 
 module.exports.renderChangeForm = (req, res) => {
@@ -49,7 +53,7 @@ module.exports.changePassword = async (req, res) => {
     User.findById(req.user._id, (err, user) => {
         // console.log(err, user);
         user.changePassword(req.body.password_former, req.body.password, err => {
-            if(err) {
+            if (err) {
                 req.flash('error', 'Your passwords do not match, please try again.');
                 res.redirect('/changePassword');
             }
@@ -70,3 +74,70 @@ module.exports.logout = (req, res, next) => {
         }
     });
 };
+
+
+module.exports.renderResetQuestForm = (req, res, next) => {
+    res.render('users/resetQuestForm');
+}
+
+module.exports.sendResetLink = async (req, res, next) => {
+    const user = await User.findOne({ username: req.body.username }); // this needs to be modified to email allowed
+    if (!user) {
+        req.flash('error', 'User does not exist, please try again.');
+        return res.redirect('/forgotPassword');
+    } else {
+        const token = await UserToken.findOne({ user: user._id });
+        if (token) await token.deleteOne();
+        const resetToken = crypto.randomBytes(32).toString('hex');
+        const hash = await bcrypt.hash(resetToken, 12, async (err, hash) => {
+            if (err) throw new ExpressError(err.message, 500);
+            else {
+                await new UserToken({
+                    user: user._id,
+                    email: user.email,
+                    token: hash,
+                    createdAt: Date.now(),
+                }).save();
+            }
+        });
+        res.render('users/forgotSimu', { resetToken, user: user._id });
+    }
+}
+
+module.exports.renderResetForm = async (req, res, next) => {
+    const passwordResetToken = await UserToken.findOne({ user: req.query.user });
+    if (!passwordResetToken) {
+        req.flash('error', 'Invalid or expired password reset token, please try it again.');
+        return res.redirect('/forgotPassword');
+    }
+    // console.log(passwordResetToken, req.query);
+    const isValid = await bcrypt.compare(req.query.token, passwordResetToken.token);
+    // console.log(isValid);
+    if (!isValid) {
+        req.flash('error', 'Invalid or expired password reset token, please try it again.');
+        return res.redirect('/forgotPassword');
+    }
+    else {
+        res.render('users/resetForm', { token: req.query.token, user: req.query.user });
+    }
+
+}
+
+module.exports.resetPassword = async (req, res, next) => {
+    const user = await User.findById(req.query.user);
+    if (!user) {
+        req.flash('error', 'Invalid token or user, please try again.');
+        res.redirect('/forgotPassword');
+    } else {
+        await user.setPassword(req.body.password, async (err, user)=> {
+            if(err) {
+                throw ExpressError('Invalid token or user, please try again.', 500);
+            }
+            else {
+                await user.save();
+            }
+        });
+        req.flash('success', 'You have successfully reset the password, remember it!');
+        res.redirect('/campgrounds');
+    }
+}
